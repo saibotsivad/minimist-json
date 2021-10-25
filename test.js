0220b7d57031f18ae53511be709369e65fceb9b3
Tobias Davis <tobias@davistobias.com>
2021-10-25 10:32:24 -0500

    test: updated tape to uvu and add dot-notation tests

--- test.js
+++ test.js
@@ -1,33 +1,41 @@
-const test = require('tape')
+const { test } = require('uvu')
+const assert = require('uvu/assert')
 const exec = require('child_process').exec
 
-test('that it works', t => {
-	t.plan(4)
-	exec('node ./bin.js --hello=world', (err, output) => {
-		t.notOk(err, 'error free')
-		const obj = JSON.parse(output)
-		t.equal(obj.hello, 'world', 'param is correct')
-		t.ok(Array.isArray(obj._), 'contains minimist array')
-		t.equal(obj._.length, 0, 'but it should be empty')
-		t.end()
+const parse = async string => new Promise((resolve, reject) => {
+	exec(string, (error, output) => {
+		if (error) reject(error)
+		else resolve(JSON.parse(output))
 	})
 })
 
-test('array-like strings just get parsed as strings', t => {
-	t.plan(1)
-	exec('node ./bin.js --thing=[1,2]', (err, output) => {
-		const obj = JSON.parse(output)
-		t.equal(obj.thing, '[1,2]', 'it is a string not an array')
-		t.end()
-	})
+test('that it works', async () => {
+	const obj = await parse('node ./bin.js --hello=world')
+	assert.equal(obj.hello, 'world', 'param is correct')
+	assert.ok(Array.isArray(obj._), 'contains minimist array')
+	assert.equal(obj._.length, 0, 'but it should be empty')
 })
 
-test('but when you specify a json ref it gets parsed', t => {
-	t.plan(2)
-	exec('node ./bin.js --json=thing -- --thing=[1,2]', (err, output) => {
-		const obj = JSON.parse(output)
-		t.ok(Array.isArray(obj.thing), 'here it is an array not a string')
-		t.equal(obj.thing[0], 1, 'with ordered properties as expected')
-		t.end()
-	})
+test('array-like strings just get parsed as strings', async () => {
+	const obj = await parse('node ./bin.js --thing=[1,2]')
+	assert.equal(obj.thing, '[1,2]', 'it is a string not an array')
 })
+
+test('but when you specify a json ref it gets parsed', async () => {
+	const obj = await parse('node ./bin.js --json=thing -- --thing=[1,2]')
+	assert.ok(Array.isArray(obj.thing), 'here it is an array not a string')
+	assert.equal(obj.thing[0], 1, 'with ordered properties as expected')
+})
+
+test('deep dot-notation properties are set', async () => {
+	const obj = await parse('node ./bin.js --car.wheel.size=big --car.color=red')
+	assert.equal(obj.car, { wheel: { size: 'big' }, color: 'red' }, 'it is a constructed object')
+})
+
+test('to be clear, array structures are not inferred', async () => {
+	const obj = await parse('node ./bin.js --car.0.size=big --car.1.size=small')
+	assert.not.equal(Array.isArray(obj.car), 'it is just a normal object')
+	assert.equal(obj.car, { 0: { size: 'big' }, 1: { size: 'small' } }, 'it looks iterable though')
+})
+
+test.run()
